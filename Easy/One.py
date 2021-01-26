16dc8da63da53b9b4d2dd5363709ccf0409aaa1c
Satya Prakash Mahapatra <satyaprakash.mahapatra@gmail.com>
2021-01-26 20:27:21 +0530

    initial_commit

---
+++ Easy/One.py
@@ -0,0 +1,26 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers
+# such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums, target):
+
+        checkTarget = {}
+
+        count = 0
+        for each in nums:
+            if checkTarget.get(target - each) is None:
+                checkTarget.update({each: count})
+                count += 1
+
+            else:
+                print([checkTarget.get(target - each), count])
+                return [checkTarget.get(target - each), count]
+
+
+if __name__ == '__main__':
+    Solution.twoSum(Solution, [2,7,11,15], 9)
+    Solution.twoSum(Solution, [3,2,4], 6)
+    Solution.twoSum(Solution, [3,3], 6)
+
